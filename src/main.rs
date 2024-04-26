322fc973526cba191f9fc6059b1ae5b598983249
RGGH <pisscompany@gmail.com>
2024-04-26 18:49:31 +0100

    add text box ready to supply remote client ip

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 #![allow(unused)]
+
+/// refer to https://github.com/RGGH/iced_tutorial/tree/main
 use ssh2::Session;
 use std::io::Read;
 use std::net::TcpStream;
 
 use iced::widget::image::Handle;
-
 use iced::{
     alignment::{Horizontal, Vertical},
     font::Family,
     widget::{
-        button, column, container, text, text::Shaping, Button, Column, Container, Image, Renderer,
-        Row, Text, Theme,
+        button, column, container, text, text::Shaping, text_input, Button, Column, Container,
+        Image, Renderer, Row, Text, Theme,
     },
-    Alignment, Color, Font, Length, Sandbox, Settings,
+    window, Alignment, Color, Font, Length, Point, Sandbox, Settings, Size,
 };
-use iced::{window, Point, Size};
 
 fn main() -> iced::Result {
     MyApp::run(Settings {
         window: window::Settings {
             size: Size {
-                width: 210.,
-                height: 220.,
+                width: 520.,
+                height: 420.,
             },
             position: window::Position::Specific(Point { x: 900., y: 120. }),
             ..window::Settings::default()
@@ -30,30 +30,60 @@ fn main() -> iced::Result {
     })
 }
 
+pub enum Status {
+    Active,
+    Hovered,
+    Pressed,
+    Disabled,
+}
+
 #[derive(Debug, Clone)]
 enum MyAppMessage {
     ButtonPressed,
+    Update6(String),
+    Submit6,
 }
 
-struct MyApp {}
+#[derive(Debug, Clone)]
+enum Message {
+    ClearStatus,
+    TextInputChanged(String),
+}
+
+struct MyApp {
+    status: String,
+    clear_button: button::State,
+    text6: String,
+}
 
 impl Sandbox for MyApp {
     type Message = MyAppMessage;
 
     fn new() -> Self {
-        Self {}
+        MyApp {
+            status: String::new(),
+            clear_button: button::State::new(),
+            text6: "ip".to_string(),
+        }
     }
     fn theme(&self) -> iced::Theme {
         iced::Theme::Dracula
     }
 
     fn title(&self) -> String {
-        String::from("My App")
+        String::from("Guissh - SSH Client")
     }
 
     fn update(&mut self, message: Self::Message) {
         match message {
-            MyAppMessage::ButtonPressed => con(),
+            MyAppMessage::ButtonPressed => {
+                Status::Pressed;
+                con()
+            }
+            MyAppMessage::Update6(s) => {
+                self.text6 = s;
+            }
+            MyAppMessage::Submit6 => todo!(),
         }
     }
 
@@ -66,14 +96,20 @@ impl Sandbox for MyApp {
             Container::new(image)
                 .width(Length::Fill)
                 .height(Length::Fill);
+        let status_bar = Text::new(&self.status).width(iced::Length::Fill).size(20);
+
+        let value = "Some text";
 
         column![
-            text("Ready?").style(Color::from_rgb(1., 0.6, 0.2)),
-            container("SSH to ThinkCentre").padding(20),
+            container("").padding(20).align_x(Horizontal::Center),
             container(button("Connect").on_press(MyAppMessage::ButtonPressed))
                 .width(Length::Fill)
                 .align_x(Horizontal::Center),
-            Row::new().push(background) //.push(foreground)
+            Row::new().push(background),
+            Row::new().padding(30).push(status_bar),
+            text("Enter IP address").style(Color::from_rgb(1., 0.6, 0.2)),
+            text_input("Enabled text input", self.text6.as_str())
+                .on_input(|s| MyAppMessage::Update6(s)),
         ]
         .into()
     }
@@ -87,17 +123,20 @@ fn con() {
     sess.handshake().unwrap();
 
     // Try to authenticate with the first identity in the agent.
+    // ssh certificate already on remote machine
     sess.userauth_agent("rag").unwrap();
 
     // Make sure we succeeded
     assert!(sess.authenticated());
 
+    // linux/unix 'ls'
     let mut channel = sess.channel_session().unwrap();
     channel.exec("ls").unwrap();
     let mut s = String::new();
     channel.read_to_string(&mut s).unwrap();
     println!("{}", s);
 
+    // linux/unix 'uname -a'
     let mut channel = sess.channel_session().unwrap();
     channel.exec("uname -a").unwrap();
     let mut s = String::new();
