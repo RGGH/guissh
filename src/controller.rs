413058cb3ff5fe9a08385305d47fab5dd82bba8c
RGGH <pisscompany@gmail.com>
2024-04-29 14:33:59 +0100

    move ssh into controller

---
+++ src/controller.rs
@@ -0,0 +1,42 @@
+use std::io::Read;
+use crate::TcpStream;
+use crate::Session;
+
+
+pub fn con(tx: &str,user:&str) {
+    // Connect to the remote SSH server
+    let tcp = TcpStream::connect(tx).unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+
+    // Try to authenticate with the first identity in the agent.
+    // ssh certificate already on remote machine
+    sess.userauth_agent(user).unwrap();
+
+    // Make sure we succeeded
+    assert!(sess.authenticated());
+
+    // linux/unix 'ls'
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec("ls").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    println!("{}", s);
+
+    // linux/unix 'uname -a'
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec("uname -a").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    println!("{}", s);
+
+    let _ = channel.wait_close();
+
+    match channel.exit_status() {
+        Ok(0) => println!("{:?}", "Quit ok!"),
+        _ => println!("{:?}", "error closing conn"),
+    }
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![allow(unused)]
+use controller::con;
+mod controller;
 
 /// refer to https://github.com/RGGH/iced_tutorial/tree/main
 use ssh2::Session;
@@ -126,40 +128,3 @@ impl Sandbox for MyApp {
     }
 }
 
-fn con(tx: &str,user:&str) {
-    // Connect to the remote SSH server
-    let tcp = TcpStream::connect(tx).unwrap();
-    let mut sess = Session::new().unwrap();
-    sess.set_tcp_stream(tcp);
-    sess.handshake().unwrap();
-
-    // Try to authenticate with the first identity in the agent.
-    // ssh certificate already on remote machine
-    sess.userauth_agent(user).unwrap();
-
-    // Make sure we succeeded
-    assert!(sess.authenticated());
-
-    // linux/unix 'ls'
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("ls").unwrap();
-    let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    println!("{}", s);
-
-    // linux/unix 'uname -a'
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("uname -a").unwrap();
-    let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    println!("{}", s);
-
-    let _ = channel.wait_close();
-
-    match channel.exit_status() {
-        Ok(0) => println!("{:?}", "Quit ok!"),
-        _ => println!("{:?}", "error closing conn"),
-    }
-
-
-}
